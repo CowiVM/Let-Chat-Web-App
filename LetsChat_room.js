9d44bd438f8359e976cd8957344823e2a3adaae3
CowiVM <72988033+CowiVM@users.noreply.github.com>
2020-12-11 21:09:11 +0530

    Add files via upload

--- LetsChat_room.js
+++ LetsChat_room.js
@@ -1,1 +1,15 @@
-function addUser()
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAMphxsZ4sHtm2RdDP51RzMIb5HHXulRPA",
+    authDomain: "letschat-2ae6f.firebaseapp.com",
+    databaseURL: "https://letschat-2ae6f-default-rtdb.firebaseio.com",
+    projectId: "letschat-2ae6f",
+    storageBucket: "letschat-2ae6f.appspot.com",
+    messagingSenderId: "455702379322",
+    appId: "1:455702379322:web:49df8a11150e7bf3c92e03",
+    measurementId: "G-XX1BMR9QE0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
