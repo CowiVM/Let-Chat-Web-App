2f6ccc7f0ca523836d3992a17e4f4899beff1708
CowiVM <72988033+CowiVM@users.noreply.github.com>
2020-12-16 15:56:39 +0530

    Add files via upload

--- LetsChat_room.js
+++ LetsChat_room.js
@@ -1,4 +1,3 @@
-<script>
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
@@ -13,3 +12,32 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  user_name = localStorage.getItem("user_name")
+
+  document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!"
+
+  function addRoom() {
+    room = document.getElementById("room_name").value;
+    firebase.database().ref("/").child(room).update({
+      purpose: "New Room"
+    });
+
+    function getData() {
+    firebase.database().ref("/").on('value',
+    function(snapshot) {document.getElementById("output").innerHTML = "";
+    snapshot.forEach(function(childSnapshot){
+      childKey = childSnapshot.key;
+      Room_names = childKey;
+    
+      row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+}
+getData();
+
+function redirectToRoomName(name) {
+  localStorage.setItem("Room_Name", name);
+  window.location = "kwitter_page.html"
+}
